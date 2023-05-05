ce1754f4a334f795d3d204fec847305a483e5a12
dangphuong2003 <114977514+dangphuong2003@users.noreply.github.com>
2023-05-05 07:56:24 +0700

    test

--- app/src/main/java/edu/huflit/app_do_an_vat/IntroActivity.java
+++ app/src/main/java/edu/huflit/app_do_an_vat/IntroActivity.java
@@ -2,6 +2,7 @@ package edu.huflit.app_do_an_vat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+
     }
 }
